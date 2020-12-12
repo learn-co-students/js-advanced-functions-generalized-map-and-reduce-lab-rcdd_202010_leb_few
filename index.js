e0bc7d2786b72a2074c9bf077a15617615eabb4a
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-12 09:27:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(array, fx){
+  let arr=[]
+  for(let i=0;i <array.length;i++){
+    arr.push(fx(array[i]));
+  }
+  return arr;
+}
+
+function reduce(array, fx, p){
+ let a= (!!p) ? p : array[0];
+ 
+ for ( let i=(!!p) ? 0 : 1 ; i< array.length;i++){
+    a = fx(array[i], a)
+ }
+ 
+ return a;
+}
+
+
+
