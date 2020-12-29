f840657e72870768f048874bc7088b0a33299a29
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-29 06:22:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(arr, funct) {
+  let a = []
+
+  for (let i = 0; i < arr.length; i++) {
+    let b = arr[i]
+    a.push(funct(b))
+  }
+
+  return a;
+}
+
+function reduce(arr, funct, start){
+  let r = (start) ? start : arr[0]
+  let i = (start) ? 0 : 1
+
+  for (i; i < arr.length; i++) {
+    r = funct(arr[i], r)
+  }
+
+  return r;
+}
