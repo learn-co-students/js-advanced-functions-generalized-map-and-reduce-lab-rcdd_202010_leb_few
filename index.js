8aab7e55116b269a99f096e270d858c91da10542
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-30 19:17:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(arr, fct) {
+  let r = []
+  for (let i = 0; i < arr.length; i++) {
+    r.push(fct(arr[i]))
+  }
+  return r;
+}
+function reduce(arr, fct, starting){
+  let i;
+  if(!starting){
+    starting = arr[0];
+   i =1;
+  }
+  else{
+      i=0;
+  }
+  let r=starting;
+  for (i; i < arr.length; i++) {
+    r = fct(arr[i], r)
+  }
+  return r;
+}
